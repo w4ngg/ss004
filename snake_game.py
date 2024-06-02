8afe5ff20985a8c29e25e73dca829305958eacb1
UIT-23520264 <23520264@gm.uit.edu.vn>
2024-06-03 01:30:14 +0700

    init commit

--- snake_game.py
+++ snake_game.py
@@ -45,3 +45,35 @@ direction = 'RIGHT'
 change_to = direction
 
 ##NOTE for Đạt, làm windows khi vào game và kết thúc trò chơi, âm thanh
+start_bg = transform.scale(image.load('start_bg.png'), (window_x, window_y))
+end_bg = transform.scale(image.load('end.jpg'), (window_x, window_y))
+start_sound = pygame.mixer.Sound('start_sound.mp3')
+end_sound = pygame.mixer.Sound('end_sound.wav.mp3')
+eat_sound = pygame.mixer.Sound('eat.wav.mp3')
+# khai bao game window
+pygame.display.set_caption('Snacks Game')
+game_window = pygame.display.set_mode((window_x, window_y))
+
+def game_over():
+    # Gọi hàm end_game
+    end_game()
+def end_game():
+    end_sound.play()
+    while True:
+        game_window.blit(end_bg,(0,0))
+        font = pygame.font.SysFont('times new roman', 50)
+        game_over_surface = font.render('Your Score is : ' + str(score), True, black)
+        instruction_surface = font.render('Press ESC to quit', True, black)
+        game_over_rect = game_over_surface.get_rect()
+        instruction_rect = instruction_surface.get_rect()
+        game_over_rect.midtop = (window_x / 2, window_y / 4)
+        instruction_rect.midtop = (window_x / 2, window_y / 2)
+        game_window.blit(game_over_surface, game_over_rect)
+        game_window.blit(instruction_surface, instruction_rect)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    end_sound.stop()
+                    pygame.quit()
+                    quit()
